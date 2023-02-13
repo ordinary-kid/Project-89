3dbf0a6fa6c79b6c946c9848bcec75342453b245
ordinary-kid <103087378+ordinary-kid@users.noreply.github.com>
2023-02-12 19:09:13 -0800

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -7,3 +7,14 @@ function addUser(){
 
     window.location = "game_page.html";
 }
+function adduser() {
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location.replace("game_page.html");
+}
+
+
